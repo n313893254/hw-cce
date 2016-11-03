ce108b42e581c78d19ba07e7387da8ebee146519
Woo Cubic <iampurse@vip.qq.com>
2016-11-03 18:01:46 +0800

    add keypair apis

---
+++ examples/ECS/keypair-list-keypairs.js
@@ -0,0 +1,18 @@
+/**
+ * 查询SSH密钥列表
+ * https://support.hwclouds.com/api-ecs/zh-cn_topic_0020212676.html
+ */
+var HW = require('../../index.js')
+
+// 获取配置项，需要修改成你自己的AK，SK，ProjectId
+var config = require('../config.json')
+// 初始化 ECS Client
+var ECSClient = new HW.ECS(config)
+
+ECSClient.listKeypairs(function (err, response) {
+  if (!err && response.ok) {
+    console.log(JSON.stringify(response.body, null, 2))
+  } else {
+    console.log(err)
+  }
+})
