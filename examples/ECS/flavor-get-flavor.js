fc56f28f4b631bcc1ab22249f9ead33018798b2c
n313893254 <n313893254@126.com>
2018-06-20 13:58:15 +0800

    udpate

--- examples/ECS/flavor-get-flavor.js
+++ examples/ECS/flavor-get-flavor.js
@@ -13,16 +13,17 @@ var ECSClient = new HW.ECS(config)
 // 查询云服务器规格详情列表
 ECSClient.listFlavorDetails({}, function (err, response) {
   if (!err && response.ok) {
-    var flavors = response.body.flavors
-    var flavorId = flavors[0].id  // 获取flavorId
+    console.log(response.body)
+    // var flavors = response.body.flavors
+    // var flavorId = flavors[0].id  // 获取flavorId
 
     // 调用接口获取flavor详情
-    ECSClient.getFlavor(flavorId, function (err, response) {
-      if (!err && response.ok) {
-        console.log(JSON.stringify(response.body, null, 2))
-      } else {
-        console.log(err)
-      }
-    })
+    // ECSClient.getFlavor(flavorId, function (err, response) {
+    //   if (!err && response.ok) {
+    //     console.log(JSON.stringify(response.body, null, 2))
+    //   } else {
+    //     console.log(err)
+    //   }
+    // })
   }
 })
